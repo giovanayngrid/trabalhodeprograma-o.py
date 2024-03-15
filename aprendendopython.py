6482bc920934e8666470f529080cb336e3925f7a
giovanayngrid <161529434+giovanayngrid@users.noreply.github.com>
2024-03-14 21:23:35 -0300

    Create aprendendopython.py

---
+++ aprendendopython.py
@@ -0,0 +1,10 @@
+a=float(input('digite o primeiro número:'))
+b=float(input('dgite o segundo número:'))
+
+if a>b:
+  print('o maior número é:', a or b )
+elif a==b:
+  print('os números são iguais')
+else:
+ print('o menor número é:', a or b)
+
